67af0913881ab7e76e48aa3e6ea8ba8b7c73e984
song_2010040402102@163.com <song_2010040402102@163.com>
2018-07-27 18:53:48 +0800

    adjust file struct

--- ai_mj/rule_b_test.go
+++ ai_mj/rule_b_test.go
@@ -1,11 +1,11 @@
-package mahjong
+package ai_mj
 
 import (
 	"testing"
 )
 
 func BenchmarkCheckHu(b *testing.B) {
-	aiCards := []aiCard{
+	aiCards := []AICard{
 		{COLOR_WAN*MAHJONG_MASK + MAHJONG_1, 3},
 		{COLOR_WAN*MAHJONG_MASK + MAHJONG_2, 1},
 		{COLOR_WAN*MAHJONG_MASK + MAHJONG_3, 1},
@@ -18,15 +18,15 @@ func BenchmarkCheckHu(b *testing.B) {
 	}
 	for i := 0; i < b.N; i++ {
 		for j := 0; j < len(aiCards); j++ {
-			aiCards[j].num++
+			aiCards[j].Num++
 			CheckHu(aiCards)
-			aiCards[j].num--
+			aiCards[j].Num--
 		}
 	}
 }
 
 func BenchmarkCheckHuForLZ(b *testing.B) {
-	aiCards := []aiCard{
+	aiCards := []AICard{
 		{COLOR_WAN*MAHJONG_MASK + MAHJONG_1, 3},
 		{COLOR_WAN*MAHJONG_MASK + MAHJONG_2, 1},
 		{COLOR_WAN*MAHJONG_MASK + MAHJONG_3, 1},
@@ -39,89 +39,89 @@ func BenchmarkCheckHuForLZ(b *testing.B) {
 		{COLOR_TIAO*MAHJONG_MASK + MAHJONG_1, 0},
 	}
 	for i := 0; i < b.N; i++ {
-		aiCards[len(aiCards)-1].num = 1
+		aiCards[len(aiCards)-1].Num = 1
 		for j1 := 0; j1 < len(aiCards)-1; j1++ {
-			aiCards[j1].num--
+			aiCards[j1].Num--
 			CheckHuForLZ(aiCards, COLOR_TIAO*MAHJONG_MASK+MAHJONG_1)
-			aiCards[j1].num++
+			aiCards[j1].Num++
 		}
 
-		aiCards[len(aiCards)-1].num = 2
+		aiCards[len(aiCards)-1].Num = 2
 		for j1 := 0; j1 < len(aiCards)-1; j1++ {
-			if aiCards[j1].num == 0 {
+			if aiCards[j1].Num == 0 {
 				continue
 			}
-			aiCards[j1].num--
+			aiCards[j1].Num--
 			for j2 := j1; j2 < len(aiCards)-1; j2++ {
-				if aiCards[j2].num == 0 {
+				if aiCards[j2].Num == 0 {
 					continue
 				}
-				aiCards[j2].num--
+				aiCards[j2].Num--
 				CheckHuForLZ(aiCards, COLOR_TIAO*MAHJONG_MASK+MAHJONG_1)
-				aiCards[j2].num++
+				aiCards[j2].Num++
 			}
-			aiCards[j1].num++
+			aiCards[j1].Num++
 		}
 
-		aiCards[len(aiCards)-1].num = 3
+		aiCards[len(aiCards)-1].Num = 3
 		for j1 := 0; j1 < len(aiCards)-1; j1++ {
-			if aiCards[j1].num == 0 {
+			if aiCards[j1].Num == 0 {
 				continue
 			}
-			aiCards[j1].num--
+			aiCards[j1].Num--
 			for j2 := j1; j2 < len(aiCards)-1; j2++ {
-				if aiCards[j2].num == 0 {
+				if aiCards[j2].Num == 0 {
 					continue
 				}
-				aiCards[j2].num--
+				aiCards[j2].Num--
 				for j3 := j2; j3 < len(aiCards)-1; j3++ {
-					if aiCards[j3].num == 0 {
+					if aiCards[j3].Num == 0 {
 						continue
 					}
-					aiCards[j3].num--
+					aiCards[j3].Num--
 					CheckHuForLZ(aiCards, COLOR_TIAO*MAHJONG_MASK+MAHJONG_1)
-					aiCards[j3].num++
+					aiCards[j3].Num++
 				}
-				aiCards[j2].num++
+				aiCards[j2].Num++
 			}
-			aiCards[j1].num++
+			aiCards[j1].Num++
 		}
 
-		aiCards[len(aiCards)-1].num = 4
+		aiCards[len(aiCards)-1].Num = 4
 		for j1 := 0; j1 < len(aiCards)-1; j1++ {
-			if aiCards[j1].num == 0 {
+			if aiCards[j1].Num == 0 {
 				continue
 			}
-			aiCards[j1].num--
+			aiCards[j1].Num--
 			for j2 := j1; j2 < len(aiCards)-1; j2++ {
-				if aiCards[j2].num == 0 {
+				if aiCards[j2].Num == 0 {
 					continue
 				}
-				aiCards[j2].num--
+				aiCards[j2].Num--
 				for j3 := j2; j3 < len(aiCards)-1; j3++ {
-					if aiCards[j3].num == 0 {
+					if aiCards[j3].Num == 0 {
 						continue
 					}
-					aiCards[j3].num--
+					aiCards[j3].Num--
 					for j4 := j3; j4 < len(aiCards)-1; j4++ {
-						if aiCards[j4].num == 0 {
+						if aiCards[j4].Num == 0 {
 							continue
 						}
-						aiCards[j4].num--
+						aiCards[j4].Num--
 						CheckHuForLZ(aiCards, COLOR_TIAO*MAHJONG_MASK+MAHJONG_1)
-						aiCards[j4].num++
+						aiCards[j4].Num++
 					}
-					aiCards[j3].num++
+					aiCards[j3].Num++
 				}
-				aiCards[j2].num++
+				aiCards[j2].Num++
 			}
-			aiCards[j1].num++
+			aiCards[j1].Num++
 		}
 	}
 }
 
 func BenchmarkCheckTing(b *testing.B) {
-	aiCards := []aiCard{
+	aiCards := []AICard{
 		{COLOR_WAN*MAHJONG_MASK + MAHJONG_1, 3},
 		{COLOR_WAN*MAHJONG_MASK + MAHJONG_2, 1},
 		{COLOR_WAN*MAHJONG_MASK + MAHJONG_3, 1},
@@ -138,7 +138,7 @@ func BenchmarkCheckTing(b *testing.B) {
 }
 
 func BenchmarkCheckTingForLZ(b *testing.B) {
-	aiCards := []aiCard{
+	aiCards := []AICard{
 		 {COLOR_WAN*MAHJONG_MASK + MAHJONG_1, 2},
 		 {COLOR_WAN*MAHJONG_MASK + MAHJONG_2, 1},
 		 {COLOR_WAN*MAHJONG_MASK + MAHJONG_3, 3},
